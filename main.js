86926f50be1c41e8cb62787c5c209becb9d0eb59
Muthu Raman <88555538+Muthuram05@users.noreply.github.com>
2023-01-08 21:02:22 +0530

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-
-function openBar(cityName) {
-  var i;
-  var x = document.getElementsByClassName("TabView");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  document.getElementById(cityName).style.display = "block";  
-}
